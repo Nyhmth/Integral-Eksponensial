842a6020a23c9084bbc57e5a1262f6862dcb11c4
nyh <114615908+Nyhmth@users.noreply.github.com>
2022-11-17 15:11:27 +0700

    Add files via upload

---
+++ integral eksponensial.py
@@ -0,0 +1,32 @@
+from scipy import integrate
+import numpy as np
+import matplotlib.pyplot as plotlib
+
+#Define parameters
+x_start = 1
+x_stop = 5
+x_steps_interval = 0.01
+
+#Define an array of data points
+x_values = np.arange(x_start, x_stop, x_steps_interval)
+y_values = 2 *  np.exp(x_values)
+
+#plot the function curve
+plotlib.plot(x_values, y_values)
+
+#define a lambda function for integration over x values
+integration_function = lambda x : 2 * np.exp(-x)
+
+# Calculate the integral.
+integral, error = integrate.quad(integration_function, x_start, x_stop)
+
+# Print the integration results.
+print("Integral Value:")
+print(integral)
+print("Integration Error:")
+print(error)
+
+# Display the plot.
+plotlib.xlabel('x')
+plotlib.ylabel('y')
+plotlib.show()
